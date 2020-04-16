028e7731107e6e9bfb114831e3a26c5f5ed36805
adnanhamkaa <adnanhamkaa@gmail.com>
2020-04-16 11:46:21 +0700

    add student controller + view

--- CourseOne/Context/DatabaseContext.cs
+++ CourseOne/Context/DatabaseContext.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 
 namespace CourseOne.Context
 {
-    public class DatabaseContext 
+    public class DatabaseContext : DbContext
     {
+        public System.Data.Entity.DbSet<CourseOne.Models.Student> Students { get; set; }
     }
 }

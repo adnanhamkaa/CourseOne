2da5f0bc1b659eebaacde76288d2e883302feced
adnanhamkaa <adnanhamkaa@gmail.com>
2020-04-16 11:29:00 +0700

    Add Models and create dbcontext

---
+++ CourseOne/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CourseOne.Models
+{
+    public class Student
+    {
+        [Key]
+        public Guid StudentId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+}
